6b01a7897a6f395da453c871cee6bc36751764d3
straksi <kk095@yandex.ru>
2024-12-18 16:15:38 +0300

    chng

---
+++ portfolio/js/data.js
@@ -0,0 +1,36 @@
+const blocks = {
+  block1: {
+    id: 'block1',
+    title: 'Коммерческие проекты',
+    portfolio: [
+      { src: 'img/academpharm.jpg', url: 'https://academpharm.by/', alt: 'Academpharm' },
+      { src: 'img/delta.jpg', url: 'https://delta-mebel.by/', alt: 'Delta' },
+      { src: 'img/smileplaza.jpg', url: 'https://smileplaza.by/', alt: 'Smileplaza' },
+	    { src: 'img/silamen.jpg', url: 'https://www.silamen.by/', alt: 'Silamen' },
+      { src: 'img/pro-tromboz.jpg', url: 'https://pro-tromboz.by/', alt: 'Pro-tromboz' },
+      { src: 'img/friendsfoundation.jpg', url: 'https://friendsfoundation.ru/en/', alt: 'Fond' },
+      { src: 'img/to4ka.jpg', url: 'https://to4ka.estate/', alt: 'To4ka' },
+      { src: 'img/less.jpg', url: 'https://less-shop.by/', alt: 'Less' },
+      { src: 'img/alginomax.jpg', url: 'https://alginomax.by/', alt: 'Alginomax' },
+      { src: 'img/artroxib.jpg', url: 'https://academpharm.by/net-boli-est-dvizhenie-artroksib', alt: 'Artroksib' },
+      { src: 'img/forzhekt.jpg', url: 'https://academpharm.by/forzhekt', alt: 'Forzhekt' },
+      { src: 'img/kurkusept.jpg', url: 'https://academpharm.by/kurkusept', alt: 'Kurkusept' },
+
+      { src: 'img/glmkk.jpg', url: 'https://glmkk.by/', alt: 'Glmkk' },
+      // { src: 'img/youcan.jpg', url: 'https://example4.com', alt: 'Youcan' },
+      { src: 'img/mamatest.jpg', url: 'https://mamatest.by/', alt: 'Mamatest' },
+      { src: 'img/nft.procharity.jpg', url: 'https://nft.procharity.ru/', alt: 'Nft.procharity' },
+      { src: 'img/ph.jpg', url: 'https://peha-haft.by/', alt: 'Peha-haft' },
+      { src: 'img/oko-center.jpg', url: 'https://oko-center.by/', alt: 'OKO' },
+      { src: 'img/sh.jpg', url: 'https://shuraav.by/', alt: 'Shuraav' },
+      { src: 'img/webmodern.jpg', url: 'https://web-modern.by/', alt: 'Web-modern' },
+      
+      { src: 'img/bs.jpg', url: 'https://publichealth.by/', alt: 'Publichealth' },
+      { src: 'img/dilandiss.jpg', url: 'https://dilandiss.by/', alt: 'Dilandiss' },
+      { src: 'img/et.jpg', url: 'https://e-term.by/', alt: 'E-term' },
+      { src: 'img/sp.jpg', url: 'https://smartprint.by/', alt: 'Smartprint' },
+      { src: 'img/galteyapharm.jpg', url: 'https://galteyapharm.by/', alt: 'Galteyapharm' },
+      { src: 'img/gt.jpg', url: 'https://givingtuesday.by/', alt: 'Givingtuesday' },
+    ]
+  }
+}; 
